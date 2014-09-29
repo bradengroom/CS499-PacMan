02a1782b53d65053df39ad228c8010e52b3e12ac
sdum9213 <sheldondumas@yahoo.com>
2014-09-29 17:52:02 -0500

    test to create board for game

---
+++ src/test/Board.java
@@ -0,0 +1,8 @@
+ public static void main(String[] args) {
+        int p = Integer.parseInt(args[0]);
+        Board b = new Maze(N);
+        StdDraw.show(0);
+        b.build();
+        
+        
+        }
