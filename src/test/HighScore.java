624c257b032c0609694504a701c4d5bcdc5d4dbb
sdum9213 <sheldondumas@yahoo.com>
2014-09-29 21:13:43 -0500

    Update HighScore.java

--- src/test/HighScore.java
+++ src/test/HighScore.java
@@ -1,13 +1,18 @@
 
+import static org.junit.Assert.*;
 
-public class HighScore {
+import org.junit.Test;
 
+public class HighScore() {
 	
- public static void main(String args[]) {
-HighScore s = new HighScore(args[1]);
-    if (args[0].equals("base")) {
+@Test
+	Score s = new Score();
+    if (args[0].equals("set")) {
         s.setHighScore(Integer.parseInt(args[2]));
     } else {
-        System.out.println("score = "+ s.getHighScore())
+        System.out.println("score = "+ s.getHighScore());
+        
+        assert(s.HighScore());
         
         }
+}
