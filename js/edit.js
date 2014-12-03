fd4d515ee0449052a9848f26a05f381e2840763b
Sheldon Dumas <sheldondumas@yahoo.com>
2014-12-03 15:11:25 -0600

    added map sharing

--- js/edit.js
+++ js/edit.js
@@ -5,6 +5,7 @@ var spriteSize = 20,
     speed = 1.6;
 
 var fileUploaded = false;
+var playingShared = false;
 var map = "";
 
 function initializeGame(width, height) {
@@ -68,88 +69,103 @@ function initializeGame(width, height) {
 }
 
 function loadMap(levelMap) {
-    console.log("called");
-    //split file into lines
-    var lines = levelMap.split("\n");
 
-    //call initializeGame
-    initializeGame(lines[0].length, lines.length);
-
-    //loop over each line
-    $.each(lines, function (y, line) {
-
-        var characters = line.split(""),
-            bitmap = $.map(characters, function (char) {
-                if (['W', '1', '2', '3', '4', '6', '7', '8', '9', 'v', '^', '<', '>', '|', '-'].indexOf(char) > -1) {
-                    return 1;
-                } else if (char === 'G') {
-                    return 2;
-                }
-                return 0;
-            });
-        levelBitMap.push(bitmap);
-
-        //split each line into characters and loop over each character
-        $.each(characters, function (x, char) {
-
-            //set the x and y coordinates for the current item
-            var xCoord = x * spriteSize,
-                yCoord = y * spriteSize;
-
-            //match and create the appropriate entity
-            if (char === 'W') {
-                Crafty.e("Wall").create(xCoord, yCoord, "wall");
-            } else if (char === 'Z') {
-                Crafty.e("Score");
-            } else if (char === 'X') {
-                Crafty.e("Lives");
-            } else if (char === 'p') {
-                Crafty.e("Pellet").create(xCoord, yCoord);
-            } else if (char === 'S') {
-                Crafty.e("PowerUp").create(xCoord, yCoord);
-            } else if (char === 'M') {
-                Crafty.e("Pacman").create(xCoord, yCoord);
-            } else if (char === 'B') {
-                Crafty.e("Ghost").create(xCoord, yCoord, speed).setAI("Blinky");
-            } else if (char === 'I') {
-                Crafty.e("Ghost").create(xCoord, yCoord, speed).setAI("Inky");
-            } else if (char === 'P') {
-                Crafty.e("Ghost").create(xCoord, yCoord, speed).setAI("Pinky");
-            } else if (char === 'C') {
-                Crafty.e("Ghost").create(xCoord, yCoord, speed).setAI("Clyde");
-            } else if (char === 'G') { //gate
-                Crafty.e("Gate").create(xCoord, yCoord, "gate");
-            } else if (char === '1') { //bottom left corner
-                Crafty.e("Wall").create(xCoord, yCoord, "blCr");
-            } else if (char === '2') { //T
-                Crafty.e("Wall").create(xCoord, yCoord, "regT");
-            } else if (char === '3') { //bottom right corner
-                Crafty.e("Wall").create(xCoord, yCoord, "brCr");
-            } else if (char === '4') { //left T
-                Crafty.e("Wall").create(xCoord, yCoord, "lftT");
-            } else if (char === '6') { //right T
-                Crafty.e("Wall").create(xCoord, yCoord, "ritT");
-            } else if (char === '7') { //top left corner
-                Crafty.e("Wall").create(xCoord, yCoord, "tlCr");
-            } else if (char === '8') { //inverted T
-                Crafty.e("Wall").create(xCoord, yCoord, "invT");
-            } else if (char === '9') { //top right corner
-                Crafty.e("Wall").create(xCoord, yCoord, "trCr");
-            } else if (char === 'v') { //bottom cap
-                Crafty.e("Wall").create(xCoord, yCoord, "bCap");
-            } else if (char === '^') { //top cap
-                Crafty.e("Wall").create(xCoord, yCoord, "tCap");
-            } else if (char === '-') { //horizontal
-                Crafty.e("Wall").create(xCoord, yCoord, "horz");
-            } else if (char === '|') { //lateral
-                Crafty.e("Wall").create(xCoord, yCoord, "late");
-            } else if (char === '<') { //left cap
-                Crafty.e("Wall").create(xCoord, yCoord, "lCap");
-            } else if (char === '>') { //right cap
-                Crafty.e("Wall").create(xCoord, yCoord, "rCap");
+    if (playingShared) {
+        $.get(
+            "http://104.131.22.227/upvote/" + mapid,
+            function (mapData) {
+                playingShared = false;
+                levelMap = mapData.data;
+                load();
             }
+        );
+    } else {
+        load();
+    }
+
+    function load() {
+        //split file into lines
+        var lines = levelMap.split("\n");
+
+        //call initializeGame
+        initializeGame(lines[0].length, lines.length);
+
+        //loop over each line
+        $.each(lines, function (y, line) {
+
+            var characters = line.split(""),
+                bitmap = $.map(characters, function (char) {
+                    if (['W', '1', '2', '3', '4', '6', '7', '8', '9', 'v', '^', '<', '>', '|', '-'].indexOf(char) > -1) {
+                        return 1;
+                    } else if (char === 'G') {
+                        return 2;
+                    }
+                    return 0;
+                });
+            levelBitMap.push(bitmap);
+
+            //split each line into characters and loop over each character
+            $.each(characters, function (x, char) {
+
+                //set the x and y coordinates for the current item
+                var xCoord = x * spriteSize,
+                    yCoord = y * spriteSize;
+
+                //match and create the appropriate entity
+                if (char === 'W') {
+                    Crafty.e("Wall").create(xCoord, yCoord, "wall");
+                } else if (char === 'Z') {
+                    Crafty.e("Score");
+                } else if (char === 'X') {
+                    Crafty.e("Lives");
+                } else if (char === 'p') {
+                    Crafty.e("Pellet").create(xCoord, yCoord);
+                } else if (char === 'S') {
+                    Crafty.e("PowerUp").create(xCoord, yCoord);
+                } else if (char === 'M') {
+                    Crafty.e("Pacman").create(xCoord, yCoord);
+                } else if (char === 'B') {
+                    Crafty.e("Ghost").create(xCoord, yCoord, speed).setAI("Blinky");
+                } else if (char === 'I') {
+                    Crafty.e("Ghost").create(xCoord, yCoord, speed).setAI("Inky");
+                } else if (char === 'P') {
+                    Crafty.e("Ghost").create(xCoord, yCoord, speed).setAI("Pinky");
+                } else if (char === 'C') {
+                    Crafty.e("Ghost").create(xCoord, yCoord, speed).setAI("Clyde");
+                } else if (char === 'G') { //gate
+                    Crafty.e("Gate").create(xCoord, yCoord, "gate");
+                } else if (char === '1') { //bottom left corner
+                    Crafty.e("Wall").create(xCoord, yCoord, "blCr");
+                } else if (char === '2') { //T
+                    Crafty.e("Wall").create(xCoord, yCoord, "regT");
+                } else if (char === '3') { //bottom right corner
+                    Crafty.e("Wall").create(xCoord, yCoord, "brCr");
+                } else if (char === '4') { //left T
+                    Crafty.e("Wall").create(xCoord, yCoord, "lftT");
+                } else if (char === '6') { //right T
+                    Crafty.e("Wall").create(xCoord, yCoord, "ritT");
+                } else if (char === '7') { //top left corner
+                    Crafty.e("Wall").create(xCoord, yCoord, "tlCr");
+                } else if (char === '8') { //inverted T
+                    Crafty.e("Wall").create(xCoord, yCoord, "invT");
+                } else if (char === '9') { //top right corner
+                    Crafty.e("Wall").create(xCoord, yCoord, "trCr");
+                } else if (char === 'v') { //bottom cap
+                    Crafty.e("Wall").create(xCoord, yCoord, "bCap");
+                } else if (char === '^') { //top cap
+                    Crafty.e("Wall").create(xCoord, yCoord, "tCap");
+                } else if (char === '-') { //horizontal
+                    Crafty.e("Wall").create(xCoord, yCoord, "horz");
+                } else if (char === '|') { //lateral
+                    Crafty.e("Wall").create(xCoord, yCoord, "late");
+                } else if (char === '<') { //left cap
+                    Crafty.e("Wall").create(xCoord, yCoord, "lCap");
+                } else if (char === '>') { //right cap
+                    Crafty.e("Wall").create(xCoord, yCoord, "rCap");
+                }
+            });
         });
-    });
+    }
 }
 
 Crafty.defineScene("startScreen", function () {
@@ -298,8 +314,43 @@ function loadFile() {
     }
 }
 
+function shareMap() {
+
+    var mapName = prompt("Name your map", "newMap");
+    if (mapName != null) {
+
+        var mapData = getMap();
+
+        mapData = mapData.replace(/\n/g, "%0A");
+
+        $.get(
+            "http://104.131.22.227/upload/" + mapName + "/" + mapData,
+            console.log
+        );
+    }
+}
+
 $('#file1').change(function () {
     var filename = $('#file1').val();
     fileUploaded = true;
     loadFile();
 });
+
+function getUrlParameter(sParam) {
+    var sPageURL = window.location.search.substring(1);
+    var sURLVariables = sPageURL.split('&');
+    for (var i = 0; i < sURLVariables.length; i++) {
+        var sParameterName = sURLVariables[i].split('=');
+        if (sParameterName[0] == sParam) {
+            return sParameterName[1];
+        }
+    }
+}
+
+var mapid = getUrlParameter("id");
+
+if (typeof mapid !== 'undefined') {
+
+    playingShared = true;
+    loadCustomMap();
+}
